7b873d92476f5f335135d16d9cf7989514d8455c
Marcos Candeia <marrcooos@gmail.com>
2024-09-27 13:33:32 -0300

    Fix tailwind ref
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- fresh.config.ts
+++ fresh.config.ts
@@ -3,12 +3,11 @@ import { withActors } from "@deco/actors/hono";
 import { Deco, DecoRouteState } from "@deco/deco";
 import { framework as htmxFramework } from "@deco/deco/htmx";
 import { Hono } from "@hono/hono";
-import plugins from "deco/plugins/fresh.ts";
+import { plugins } from "deco/plugins/deco.ts";
 import "deco/runtime/htmx/FreshHeadCompat.ts";
 import { Chat } from "./actors/Chat.ts";
 import { Counter } from "./actors/Counter.ts";
 import manifest, { Manifest } from "./manifest.gen.ts";
-import tailwind from "./tailwind.config.ts";
 
 const server = new Hono<DecoRouteState<Manifest>>();
 server.use(withActors([Counter, Chat]));
@@ -25,7 +24,5 @@ export default defineConfig({
   plugins: plugins({
     manifest,
     deco,
-    // deno-lint-ignore no-explicit-any
-    tailwind: tailwind as any,
   }),
 });
